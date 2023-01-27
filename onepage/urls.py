00162c3c40e6993a20f55aada83451e6876f8a91
alireza.sadeghian <alirezasadeghian52@gmail.com>
2023-01-27 16:46:51 +0330

    Delete the extra file

--- onepage/urls.py
+++ onepage/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('onepagesite.urls')),
-    path('onepage',include('onepagesite.urls'))
 ]
 
 
