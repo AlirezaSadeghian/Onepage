db39c2e4cfa7dd2598d473a12bf0ed250032bb97
alireza.sadeghian <alirezasadeghian52@gmail.com>
2023-01-27 16:33:53 +0330

    load static and template
    load static(img,js,css,...) , load template (html),+add contex ,...

--- onepage/settings.py
+++ onepage/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'onepage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
